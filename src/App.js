695dbd7a9c9b74e9d0ac567aab04c74735a0a3c5
David Clarke <drclarke99@gmail.com>
2023-05-07 16:50:35 -0700

    adding highlights and landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Nav from './components/Nav';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
+import Landing from './components/Landing';
+import Highlights from './components/Highlights';
 
-library.add(faBars, faShoppingCart, faTimes);
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags);
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Landing />
+      <Highlights />
     </div>
   );
 }

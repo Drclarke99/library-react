27b14de5781b9a77e51fe49cda4bf7c64e628bb6
David Clarke <drclarke99@gmail.com>
2023-05-07 18:01:57 -0700

    create books page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, fa
 import Footer from './components/Footer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import Books from './pages/Books';
+import {books} from "./data.js";
 
 library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt);
 
@@ -11,11 +13,11 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <Nav>
+      <Nav />
         <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/books" element={() => <Books books={books} />} />
         </Routes>
-      </Nav>
-      <Home />
       <Footer />
     </div>
     </Router>

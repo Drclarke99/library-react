5acc45a7b1094769a8a737a49e4cd6ee00429d23
David Clarke <drclarke99@gmail.com>
2023-05-08 13:48:05 -0700

    empty state

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import EmptyCart from "../assets/empty_cart.svg";
+import { Link } from "react-router-dom";
 
 const Cart = ({cart, changeQuantity, removeItem}) => {
     const total = () => {
@@ -60,7 +62,15 @@ const Cart = ({cart, changeQuantity, removeItem}) => {
                                 })
                             }
                         </div>
-                        <div className="total">
+                        {cart.length === 0 && (<div className="cart__empty">
+                            <img src={EmptyCart} alt="" className="cart__empty--img" />
+                            <h2>You don't have any books in your cart!</h2>
+                            <Link to="/books">
+                                <button className="btn">Browse books</button>
+                            </Link>
+                            </div>
+                        )}
+                        {cart.length > 0 && (<div className="total">
                             <div className="total__item total__sub-total">
                                 <span>Subtotal</span>
                                 <span>${(total() * 0.9).toFixed(2)}</span>
@@ -78,6 +88,7 @@ const Cart = ({cart, changeQuantity, removeItem}) => {
                                 Proceed to checkout
                             </button>
                         </div>
+                        )}
                     </div>
                 </div>
             </main>

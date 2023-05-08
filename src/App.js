57af8e3081d8d5394f92ba4e65334fccd2243134
David Clarke <drclarke99@gmail.com>
2023-05-07 17:16:59 -0700

    featured books refactored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Nav from './components/Nav';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
+import Featured from './components/Featured';
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags);
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt);
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Nav></Nav>
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
